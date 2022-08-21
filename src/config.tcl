3badd93cbfd9d1dd1e9960bccc7f732fb118f5c9
Greg Davill <greg.davill@gmail.com>
2022-08-21 19:40:43 +0930

    bump PL_TARGET_DENSITY

--- src/config.tcl
+++ src/config.tcl
@@ -28,7 +28,7 @@ $::env(DESIGN_DIR)/bcd_counter.v"
 set ::env(FP_SIZING) absolute
 set ::env(DIE_AREA) "0 0 100 100"
 set ::env(FP_CORE_UTIL) 50
-set ::env(PL_TARGET_DENSITY) 0.70
+set ::env(PL_TARGET_DENSITY) 0.85
 
 # use alternative efabless decap cells to solve LI density issue
 set ::env(DECAP_CELL) "\
