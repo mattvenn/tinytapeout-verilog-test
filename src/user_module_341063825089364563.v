83cd200fd7e4fe9d2f473bcb99e75081fa390bc1
Christian Lyder Jacobsen <cljacobsen@gmail.com>
2022-08-26 22:49:09 +0200

    Add ability to change the speed

--- src/user_module_341063825089364563.v
+++ src/user_module_341063825089364563.v
@@ -5,28 +5,39 @@ module user_module_341063825089364563(
   input [7:0] io_in,
   output [7:0] io_out
 );
+  parameter COUNTER_WIDTH = 24;
 
   // using io_in[0] as clk, io_in[1] as reset
   wire clk;
   assign clk = io_in[0];
   wire reset;
   assign reset = io_in[1];
+  wire [1:0] fn;
+  assign fn = io_in[3:2];
+  wire [3:0] data;
+  assign data = io_in[7:3];
 
-  reg [21:0] counter = 0; // XXX: What is the clk freq for TT?
+  reg [COUNTER_WIDTH:0] counter = 0; // XXX: What is the clk freq for TT?
+  reg [COUNTER_WIDTH:0] counter_speed = {COUNTER_WIDTH{1'b1}};
   reg [2:0] state = 3'b000;
   reg [7:0] led_out = 0;
 
   always @(posedge clk) begin
     if (reset) begin
         counter <= 0;
+        counter_speed <= {COUNTER_WIDTH{1'b1}};
         state <= 0;
         led_out <= 8'b00000000;
+    end else if (fn == 2'b01) begin
+      counter <= 0;
+      counter_speed[COUNTER_WIDTH:COUNTER_WIDTH-4] <= data ^ 4'b1111;
     end else begin
-      if (counter == 0) begin // overflow
+      if (counter >= counter_speed) begin
+        counter <= 0;
         state <= state + 3'b001;
       end
-
-      counter <= counter + 1;
+      else
+        counter <= counter + 1;
     end
 
     case(state)
@@ -43,6 +54,4 @@ module user_module_341063825089364563(
   end
 
   assign io_out = led_out ^ 8'b11111111;
-  //assign io_out[6] = led;
-
 endmodule
