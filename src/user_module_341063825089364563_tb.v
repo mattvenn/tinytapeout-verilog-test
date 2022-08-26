482da0d2738224ed685435caf4aa59d0064459d7
Christian Lyder Jacobsen <cljacobsen@gmail.com>
2022-08-27 00:04:42 +0200

    Toggle speed in test bench

--- src/user_module_341063825089364563_tb.v
+++ src/user_module_341063825089364563_tb.v
@@ -5,6 +5,9 @@ module user_module_341063825089364563_tb;
 
 reg [7:0] io_in;
 wire [7:0] io_out;
+wire [2:0] fn;
+`define fn io_in[3:2]
+`define data io_in[7:4]
 
 user_module_341063825089364563 UUT (.io_in(io_in), .io_out(io_out));
 
@@ -14,7 +17,7 @@ initial begin
 end
 
 initial begin
-   #100_000_000; // Wait a long time in simulation units (adjust as needed).
+   #100_000; // Wait a long time in simulation units (adjust as needed).
    $display("Caught by trap");
    $finish;
  end
@@ -27,12 +30,17 @@ always begin
   #(CLK_HALF_PERIOD);
 end
 
-initial 
+initial
 begin
   #20
 	io_in[1] = 1;
 	#(CLK_HALF_PERIOD);
 	io_in[1] = 0;
+  #100
+  `data = 4'b0000;
+  `fn = 2'b01;
+  #20
+  `fn = 2'b00;
 end
 
 endmodule
