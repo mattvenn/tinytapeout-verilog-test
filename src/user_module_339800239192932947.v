335fe4a74f1335c817da51c68f2125cd322f5f24
Kamyar Mohajerani <kammoh@gmail.com>
2022-09-01 15:46:22 -0400

    fix module name

--- src/user_module_339800239192932947.v
+++ src/user_module_339800239192932947.v
@@ -6,6 +6,6 @@ module user_module_339800239192932947 (
   output [7:0] io_out
 );
 
-  mulinv mod1(.clock(1'b0), .reset(1'b0), .io_in(io_in), .io_out(io_out));
+  MulInv8 mod1(.clock(1'b0), .reset(1'b0), .io_in(io_in), .io_out(io_out));
 
 endmodule
